447a1e346f1dc909d2cd42d014af7f1cc340c5fe
777jon <jcruzdeveloper@gmail.com>
2014-12-23 04:33:38 -0500

    f400: port from d855 device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common g3
+-include device/lge/g3-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG := d855_defconfig
+TARGET_REQUIRES_BUMP := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1929823923
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
